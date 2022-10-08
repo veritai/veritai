3f0c82e3da69bed4f8d013cf45058467d01a21e2
Wesley SMith <wesleyfsmith@gmail.com>
2022-10-08 18:36:30 -0500

    adds attestation nft

--- contracts/contracts/AttestationNFT.sol
+++ contracts/contracts/AttestationNFT.sol
@@ -4,7 +4,38 @@ import "erc721a/contracts/ERC721A.sol";
 
 contract AttestationNFT is ERC721A {
     
-    
+  mapping (uint256 => uint256) soulIds;
+  mapping (uint256 => string) private tokenUris;
+  mapping (uint256 => uint256) private nftReferences; //TODO
+
+  function tokenURI(uint256 tokenId) 
+      public 
+      view
+      override
+      returns (string memory) 
+  {
+      return tokenUris[tokenId];
+  }
+
+  constructor() ERC721A("ANft", "Attestation Nft") {}
+
+  function mint(string memory uri, uint256 soulId) 
+    public 
+    returns (uint256) 
+  {
+    uint256 index = _currentIndex;
+    tokenUris[index] = uri;
+    soulIds[index] = soulId;
+    _safeMint(msg.sender, 1);
+    return index;
+  }
+
+  function getSoulId(uint256 tokenId)
+    public view
+    returns (uint256)
+  {
+    return soulIds[tokenId];
+  }
 
 }
 
