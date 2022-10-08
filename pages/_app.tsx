5dcc06fbb3d2301d5d3c5949886bc10342786b2b
Jose Erney Ospina <jose@avenfor.com>
2022-10-08 15:27:52 -0500

    ( Build )[ Wallet Connect ]: Setup Wallet connect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type {ConfigOptions} from '@web3modal/react'
+import {Web3Modal} from '@web3modal/react'
+
+const config: ConfigOptions = {
+    projectId: 'b08b940279a402deb35485e65e2a76b6',
+    theme: 'dark',
+    accentColor: 'default',
+    ethereum: {
+        appName: 'web3Modal'
+    }
+}
+
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Component {...pageProps} />
+            <Web3Modal config={config}/>
+        </>
+    )
+
 }
 
 export default MyApp
