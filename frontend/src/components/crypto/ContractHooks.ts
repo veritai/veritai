73ec0fa4ae0ee96764d79437f7874c89f82d4af8
Wesley SMith <wesleyfsmith@gmail.com>
2022-10-08 19:32:13 -0500

    sets up hooks

--- frontend/src/components/crypto/ContractHooks.ts
+++ frontend/src/components/crypto/ContractHooks.ts
@@ -1,11 +1,45 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useContractWrite } from '@web3modal/ethereum'
+import { useEffect } from 'react';
+import { useContractWrite, useWaitForTransaction } from '@web3modal/react';
+import { soulAbi } from './soul_abi';
+const { MerkleTree } = require("merkletreejs");
+const keccak256 = require("keccak256");
+
+const createMerkleTree = () => {
+  let hashes = [
+    "0x...",
+    "jdfljsdfjlkdsf",
+    "asldjalksjd"
+  ]
+
+  // Hash addresses to get the leaves
+  let leaves = hashes.map(hash => keccak256(hash))
+  
+  // Create tree
+  let merkleTree = new MerkleTree(leaves, keccak256, {sortPairs: true})
+  // Get root
+  let rootHash = merkleTree.getRoot()
+  return rootHash;
+}
+
 
 const MintId = () => {
+  const root = createMerkleTree();
+
   const { data, error, isLoading, write } = useContractWrite({
     addressOrName: '0xecb504d39723b0be0e3a9aa33d646642d1051ee1',
-    contractInterface: wagmigotchiABI,
-    functionName: 'feed'
+    contractInterface: soulAbi,
+    functionName: 'mint',
+    args: [root], //times 10 cuz decimals, gotta fix this
   })
-  write()
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  useEffect(() => {
+    write()
+  }, []);
+
+  return null;
+
 }
---
+++ frontend/src/components/crypto/soul_abi.js
@@ -0,0 +1,116 @@
+export const soulAbi = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32[]",
+        "name": "_merkleProof",
+        "type": "bytes32[]"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "_root",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "leaf",
+        "type": "bytes32"
+      }
+    ],
+    "name": "checkValidity",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getRoot",
+    "outputs": [
+      {
+        "internalType": "bytes32",
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "_root",
+        "type": "bytes32"
+      }
+    ],
+    "name": "mint",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "ownerOf",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bytes32[]",
+        "name": "_merkleProof",
+        "type": "bytes32[]"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "leaf",
+        "type": "bytes32"
+      }
+    ],
+    "name": "recoverSoul",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
